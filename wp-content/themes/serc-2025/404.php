8ce06ab49963630a6f9b8ce05e425e83f3c89295
kpettinga <kirk.pettinga@gmail.com>
2025-03-27 15:58:30 -0500

    add 404 template

--- wp-content/themes/serc-2025/404.php
+++ wp-content/themes/serc-2025/404.php
@@ -1,12 +1,21 @@
 <?php
 
 /**
- * Template Name: 404
+ * 404
  */
 ?>
 
 <?php get_header(); ?>
 
-<main>404</main>
+<main>
+	<section class="py-20 lg:py-30">
+		<div class="container">
+			<h1 class="text-h1 text-center">404</h1>
+			<h2 class="text-h3 text-center">
+				Sorry, that page could not be found.
+			</h2>
+		</div>
+	</section>
+</main>
 
 <?php get_footer(); ?>
