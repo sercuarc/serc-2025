d4278cee3d24e48eaf4e5bf3aff5bac03f1555d5
kpettinga <kirk.pettinga@gmail.com>
2025-03-27 15:09:50 -0500

    add default page template

--- wp-content/themes/serc-2025/page.php
+++ wp-content/themes/serc-2025/page.php
@@ -1,15 +1,30 @@
 <?php
 
 /**
- * Template Name: Page
+ * Default Page
  */
 ?>
 
 <?php get_header(); ?>
 
-<main class="container flex">
-	Default Page
-</main>
+<?php $image = get_the_post_thumbnail($post, 'full'); ?>
 
+<main>
+	<header class="hero <?php if ($image) : ?>hero--inverted hero--with-image<?php endif; ?>">
+		<?php if ($image = null) : ?>
+			<?php echo $image; ?>
+		<?php endif; ?>
+		<div class="container">
+			<h1 class="text-h1"><?php the_title(); ?></h1>
+		</div>
+	</header>
+	<section class="bg-white pt-12 lg:pt-16 pb-20 lg:pb-30">
+		<div class="container grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-16">
+			<div class="lg:col-span-2 wysiwyg wysiwyg-lg">
+				<?php the_content(); ?>
+			</div>
+		</div>
+	</section>
+</main>
 
 <?php get_footer(); ?>
