a1e5702c3403329dffe3859b510a2e14d847ddb0
kpettinga <kirk.pettinga@gmail.com>
2025-03-27 15:53:56 -0500

    set default single

--- wp-content/themes/serc-2025/single.php
+++ wp-content/themes/serc-2025/single.php
@@ -1,12 +1,30 @@
 <?php
 
 /**
- * Default Single
+ * Default Post
  */
 ?>
 
 <?php get_header(); ?>
 
-<main>Single: <?php the_title(); ?></main>
+<?php $image = get_the_post_thumbnail($post, 'full'); ?>
+
+<main>
+	<header class="hero <?php if ($image) : ?>hero--inverted hero--with-image<?php endif; ?>">
+		<?php if ($image = null) : ?>
+			<?php echo $image; ?>
+		<?php endif; ?>
+		<div class="container">
+			<h1 class="text-h1"><?php the_title(); ?></h1>
+		</div>
+	</header>
+	<section class="bg-white pt-12 lg:pt-16 pb-20 lg:pb-30">
+		<div class="container grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-16">
+			<div class="lg:col-span-2 wysiwyg wysiwyg-lg">
+				<?php the_content(); ?>
+			</div>
+		</div>
+	</section>
+</main>
 
 <?php get_footer(); ?>
