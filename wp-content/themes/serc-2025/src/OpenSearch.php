061dc1f03cd1f35feb2342c2cbec228a5ffac73d
kpettinga <kirk.pettinga@gmail.com>
2025-04-03 15:48:23 -0500

    Expand search capabilities

--- wp-content/themes/serc-2025/src/OpenSearch.php
+++ wp-content/themes/serc-2025/src/OpenSearch.php
@@ -28,21 +28,72 @@ class OpenSearch
 
 	public function search($params)
 	{
-		$docs = $this->client->search([
-			'index' => INDEX_NAME,
-			'body' => [
-				'size' => 5,
-				'query' => [
-					'multi_match' => [
-						'query' => 'miller',
-						'fields' => ['title^2', 'director']
-					]
+		// Build sort param
+		$sort = ['_score'];
+		if (isset($params['sort'])) {
+			$sortField = $params['sort'] . '.keyword';
+			$sortOrder = isset($params['order']) ? $params['order'] : 'desc';
+			$sort = [
+				[$sortField => ['order' => $sortOrder]],
+			];
+		}
+		// Buid page param
+		$per_page = isset($params['per_page']) ? $params['per_page'] : 20;
+		$page = isset($params['page']) ? intval($params['page']) : 1;
+		$offset = ($page - 1) * $per_page;
+
+		// Build query
+		$query = [];
+
+		// Build multi_match if a search term is provided
+		if (isset($params['query'])) {
+			$multi_match = [
+				"query" => $params["query"],
+				"fields" => ["title^2", "abstract", "description", "excerpt", "file", "file_text", "content"]
+			];
+		}
+
+		// If doc_types are provided, build the filter
+		if (isset($params['doc_types'])) {
+			$doc_types = array_map('trim', explode(',', $params['doc_types']));
+			$query["bool"] = [
+				"filter" => [
+					"terms" => ["type" => $doc_types]
 				]
-			]
+			];
+			// If a search term is also provided, add it to the query
+			if (isset($params['query'])) {
+				$query["bool"]["must"] = [
+					"multi_match" => $multi_match
+				];
+			}
+		}
+		// Otherwise, if it's a solo search query, just use the multi_match
+		else if (isset($params['query'])) {
+			$query["multi_match"] = $multi_match;
+		}
+
+		// Build search body
+		$searchBody = [
+			"size" => $per_page,
+			"from" => $offset,
+			"query" => $query,
+			"sort" => $sort
+		];
+		// Execute search
+		$docs = $this->client->search([
+			"index" => INDEX_NAME,
+			"body" => $searchBody
 		]);
+		// Return results
 		return [
-			'docs' => $docs,
-			'params' => $params
+			"params" => $params,
+			"search_body" => $searchBody,
+			"pages" => [
+				"total" => ceil($docs['hits']['total']['value'] / $per_page),
+				"current" => $page
+			],
+			"docs" => $docs,
 		];
 	}
 }
