22741b51209bc5a9c0d3c76f02ccf2441ddbafc8
kpettinga <kirk.pettinga@gmail.com>
2025-04-09 11:41:23 -0500

    add expertise and organizations taxonomies

--- wp-content/themes/serc-2025/inc/taxonomies.php
+++ wp-content/themes/serc-2025/inc/taxonomies.php
@@ -5,6 +5,8 @@
  */
 
 add_action('init', 'register_member_roles');
+add_action('init', 'register_expertise');
+add_action('init', 'register_organizations');
 
 function register_member_roles()
 {
@@ -36,4 +38,68 @@ function register_member_roles()
 	];
 
 	register_taxonomy('member_roles', ['people'], $args);
+}
+
+function register_organizations()
+{
+	$labels = [
+		'name'              => _x('Organizations', 'taxonomy general name'),
+		'singular_name'     => _x('Organization', 'taxonomy singular name'),
+		'search_items'      => __('Search Organizations'),
+		'all_items'         => __('All Organizations'),
+		'parent_item'       => __('Parent Organization'),
+		'parent_item_colon' => __('Parent Organization:'),
+		'edit_item'         => __('Edit Organization'),
+		'update_item'       => __('Update Organization'),
+		'add_new_item'      => __('Add New Organization'),
+		'new_item_name'     => __('New Organization Name'),
+		'menu_name'         => __('Organizations'),
+	];
+
+	$args = [
+		'labels'            => $labels,
+		'public'            => true,
+		'hierarchical'      => true, // True = behaves like categories, False = behaves like tags
+		'show_ui'           => true,
+		'show_in_menu'      => true,
+		'show_in_rest'      => true,
+		'show_admin_column' => true,
+		'has_archive'       => false,
+		'query_var'         => true,
+		'rewrite'           => ['slug' => 'organization'],
+	];
+
+	register_taxonomy('organizations', ['people'], $args);
+}
+
+function register_expertise()
+{
+	$labels = [
+		'name'              => _x('Expertise', 'taxonomy general name'),
+		'singular_name'     => _x('Expertise', 'taxonomy singular name'),
+		'search_items'      => __('Search Expertise'),
+		'all_items'         => __('All Expertise'),
+		'parent_item'       => __('Parent Expertise'),
+		'parent_item_colon' => __('Parent Expertise:'),
+		'edit_item'         => __('Edit Expertise'),
+		'update_item'       => __('Update Expertise'),
+		'add_new_item'      => __('Add New Expertise'),
+		'new_item_name'     => __('New Expertise Name'),
+		'menu_name'         => __('Expertise'),
+	];
+
+	$args = [
+		'labels'            => $labels,
+		'public'            => true,
+		'hierarchical'      => true, // True = behaves like categories, False = behaves like tags
+		'show_ui'           => true,
+		'show_in_menu'      => true,
+		'show_in_rest'      => true,
+		'show_admin_column' => true,
+		'has_archive'       => false,
+		'query_var'         => true,
+		'rewrite'           => ['slug' => 'expertise'],
+	];
+
+	register_taxonomy('expertise', ['people'], $args);
 }
