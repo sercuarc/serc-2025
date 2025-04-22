741fd223657039b57930b1283abc91bd9b8696db
kpettinga <kirk.pettinga@gmail.com>
2025-04-21 21:10:17 -0500

    build out single news

--- wp-content/themes/serc-2025/single.php
+++ wp-content/themes/serc-2025/single.php
@@ -3,25 +3,58 @@
 /**
  * Default Post
  */
+
+
+get_header();
+the_post();
+
+$image = get_the_post_thumbnail($post, 'full', array('class' => 'blur-sm'));
+
+$breadcrumbs = [
+	'News' => home_url('/news')
+];
+
 ?>
-
-<?php get_header(); ?>
-
-<?php $image = get_the_post_thumbnail($post, 'full'); ?>
 
 <main>
 	<header class="hero <?php if ($image) : ?>hero--inverted hero--with-image<?php endif; ?>">
-		<?php if ($image) : ?>
-			<?php echo $image; ?>
-		<?php endif; ?>
+		<?php
+		if ($image) {
+			echo $image;
+		}
+		?>
 		<div class="container">
-			<h1 class="text-h1"><?php the_title(); ?></h1>
+			<?php get_template_part('components/breadcrumbs', '', [
+				'breadcrumbs' => $breadcrumbs
+			]); ?>
+		</div>
+		<div class="container grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-16 items-center">
+			<div class="lg:col-span-2 order-2 lg:order-1">
+				<h1 class="text-h2"><?php the_title(); ?></h1>
+				<h2 class="text-h4 mt-6"><?php the_field("job_title"); ?></h2>
+				<p class="flex items-center gap-2 uppercase mt-7">
+					<?php the_date('F j, Y'); ?>
+				</p>
+			</div>
+			<div class="order-1 lg:order-2">
+				<?php the_post_thumbnail('medium', array('class' => 'border border-subtle block w-full md:w-auto')); ?>
+			</div>
 		</div>
 	</header>
-	<section class="bg-white pt-12 lg:pt-16 pb-20 lg:pb-30">
-		<div class="container grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-16">
-			<div class="lg:col-span-2 wysiwyg wysiwyg-lg">
-				<?php the_content(); ?>
+	<section class="py-12 lg:py-20">
+		<div class="container grid grid-cols-1 lg:grid-cols-4 gap-9 lg:gap-18 items-start">
+			<div class="lg:col-span-3 flex flex-col gap-10 lg:gap-20 order-2 lg:order-1">
+				<div class="wysiwyg wysiwyg-lg">
+					<?php the_content(); ?>
+				</div>
+			</div>
+			<div class="lg:col-span-1 order-1 lg:order-2">
+				<h3 class="text-h5">Share</h3>
+				<div class="flex items-center gap-4 mt-8">
+					<button title="Copy URL" class="p-2 outline-0 hover:bg-[#00000008] text-light-surface-subtle hover:text-light-surface-normal focus:text-light-surface-normal border border-subtle hover:border-normal focus:border-normal transition-all"><?php echo serc_svg('link', 'block size-7') ?><span class="sr-only">Copy link</span></button>
+					<a href="#linkedin" title="LinkedIn" class="p-2 outline-0 hover:bg-[#00000008] text-light-surface-subtle hover:text-light-surface-normal focus:text-light-surface-normal border border-subtle hover:border-normal focus:border-normal transition-all"><?php echo serc_svg('linkedin', 'block size-7') ?><span class="sr-only">LinkedIn</span></a>
+					<a href="#facebook" title="YouTube" class="p-2 outline-0 hover:bg-[#00000008] text-light-surface-subtle hover:text-light-surface-normal focus:text-light-surface-normal border border-subtle hover:border-normal focus:border-normal transition-all"><?php echo serc_svg('facebook', 'block size-7') ?><span class="sr-only">Facebook</span></a>
+				</div>
 			</div>
 		</div>
 	</section>
