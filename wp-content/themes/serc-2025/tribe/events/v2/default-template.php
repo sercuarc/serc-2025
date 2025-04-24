51aace746b829cbda2bbe97ce968b22d13c791d2
kpettinga <kirk.pettinga@gmail.com>
2025-04-24 09:48:33 -0500

    redirect events calendar template to events page

---
+++ wp-content/themes/serc-2025/tribe/events/v2/default-template.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * ------------------------- *
+ *  Default Events Template  *
+ * ------------------------- *
+ *
+ *
+ * Redirect to the Events Landing page
+ * 
+ * Make sure to set the 'Events URL Slug', in the Events Calendar settings, to something OTHER than "events" (e.g. "events-archive").
+ */
+$redirect_to = home_url('events');
+header("Location: $redirect_to", true, 301);
+die();
+/**
+ * We are redirecting to a page because we need more control over the events list landing page. 
+ * The user behavior we want goes against the grain of the way TEC wants to do things.
+ * Specifically, when it comes to filtering past events by year, it was a struggle to get TEC to display the events we wanted.
+ * Ultimately, the easiest solution was to create custom page template and query events manually.
+ * 
+ * Also, we wanted admins to be able to customize the page title and hero image.
+ * We prefer to allow them that control via a page in the CMS vs. custom fields in the events settings or somewhere else.
+ */
