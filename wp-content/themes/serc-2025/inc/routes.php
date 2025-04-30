be85963dbed7ee28555468f5d9c0ee4e0a92be0c
kpettinga <kirk.pettinga@gmail.com>
2025-04-30 12:09:08 -0500

    move brand elements to brand template

--- wp-content/themes/serc-2025/inc/routes.php
+++ wp-content/themes/serc-2025/inc/routes.php
@@ -10,6 +10,7 @@
 function serc_custom_routes()
 {
 	add_rewrite_rule('^search/?$', 'index.php?serc_search=1', 'top');
+	add_rewrite_rule('^brand/?$', 'index.php?serc_brand=1', 'top');
 	add_rewrite_rule(
 		'^documents/publications/([0-9]+)/?$',
 		'index.php?serc_document_type=publications&serc_document_id=$matches[1]',
@@ -29,6 +30,7 @@ function serc_validate_query_vars($query_vars)
 	$query_vars[] = 'query';
 	$query_vars[] = 'serc_document_type';
 	$query_vars[] = 'serc_document_id';
+	$query_vars[] = 'serc_brand';
 	return $query_vars;
 }
 add_filter('query_vars', 'serc_validate_query_vars');
@@ -40,6 +42,9 @@ function serc_load_custom_templates($template)
 	}
 	if (get_query_var('serc_document_id')) {
 		return locate_template('serc-document.php');
+	}
+	if (get_query_var('serc_brand')) {
+		return locate_template('serc-brand.php');
 	}
 	return $template;
 }
