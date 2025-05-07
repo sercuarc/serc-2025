03146eb6630ddb073f4df53e856f517e314d8156
kpettinga <kirk.pettinga@gmail.com>
2025-05-07 10:24:16 -0500

    check for blocks

--- wp-content/themes/serc-2025/page.php
+++ wp-content/themes/serc-2025/page.php
@@ -15,19 +15,23 @@
 	// Parse the blocks in the content
 	$blocks = parse_blocks($content);
 
-	// Check if the first block is a 'hero' block
-	if ($blocks[0]['blockName'] !== 'acf/hero') {
-		// Render a default hero with the page title
-		get_template_part('components/hero', null, [
-			'title' => get_the_title(),
-			'center_y' => true,
-		]);
+	if (! empty($blocks)) {
+
+		// Check if the first block is a 'hero' block
+		if ($blocks[0]['blockName'] !== 'acf/hero') {
+			// Render a default hero with the page title
+			get_template_part('components/hero', null, [
+				'title' => get_the_title(),
+				'center_y' => true,
+			]);
+		}
+
+		// Loop through each block
+		foreach ($blocks as $block) {
+			echo render_block($block);
+		}
 	}
 
-	// Loop through each block
-	foreach ($blocks as $block) {
-		echo render_block($block);
-	}
 	?>
 </main>
 
